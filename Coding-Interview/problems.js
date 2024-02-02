6a7ba8f57fdd715abee4fdc78218ca57515649ab
emonmahdi <developeremon1997@gmail.com>
2024-02-02 14:42:04 +0600

    coding problem

---
+++ Coding-Interview/problems.js
@@ -0,0 +1,15 @@
+/* // Problem 1:
+const array = [10, 20, 30, 40];
+const result = array.map((num) => num / 2).filter((num) => num >= 15);
+console.log(result); // [ 15, 20 ] */
+
+/* 
+Problem 2:  
+
+*/
+let counter = 0;
+for (var i = 1; i <= 10; i++) {
+  counter += i;
+}
+console.log(counter);
+console.log(i);
